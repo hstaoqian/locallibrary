9d0fd568751ed0d5b16d2cbcddf124f82b2d0f40
hstaoqian <hstaoqian@me.com>
2017-07-21 20:05:27 +0800

    What: add admin site & template for website & index page Why:
    How:

--- locallibrary/urls.py
+++ locallibrary/urls.py
@@ -21,6 +21,6 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^$', RedirectView.as_view('/catalog/', permanet=True)),
+    url(r'^$', RedirectView.as_view(url='/catalog/', permanent=True)),
     url(r'^catalog/', include('catalog.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
